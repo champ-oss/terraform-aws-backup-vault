cf05e6dfaeca3453c688b531aded572e6d0bf93a
justin-champtitles <justin.reeves@champtitles.com>
2025-03-10 16:40:07 -0500

    fi

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,6 @@
-
 output "vault_arn" {
   description = "The ARN of the backup vault"
-  value       = try(data.aws_ssm_parameters_by_path.this[0].values[0], null)
+  value       = try(data.aws_ssm_parameter.this[0].value, null)
 }
 
 output "ssm_arn" {
