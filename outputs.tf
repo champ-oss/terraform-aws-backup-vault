1ef728c91dd09c6260d18b16a729c1d40e1b4620
justin-champtitles <justin.reeves@champtitles.com>
2025-03-11 10:48:34 -0500

    use import

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,6 @@
 output "vault_arn" {
   description = "The ARN of the backup vault"
-  value       = try(data.aws_ssm_parameter.this[0].value, null)
+  value       = try(data.aws_ssm_parameter.this[0].insecure_value, null)
 }
 
 output "ssm_arn" {
