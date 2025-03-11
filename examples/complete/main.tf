fc4cf8494743785f0ce102ef6afb23e6e8172ea4
justin-champtitles <justin.reeves@champtitles.com>
2025-03-11 10:14:17 -0500

    import SSM

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -21,3 +21,8 @@ import {
   to = module.this.aws_backup_logically_air_gapped_vault.this[0]
   id = "backup-vault-default"
 }
+
+import {
+  to = module.this.module.ssm[0].aws_ssm_parameter.this[0]
+  id = "backup-vault-default"
+}
